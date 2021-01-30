020381ec2f022f3cb03004ca29893666ed1759e8
Rohit <r4rohit002@gmail.com>
2021-01-30 03:36:47 -0700

    Added crud operation

--- src/main/java/com/rohitksingh/springbootjavabrains/TopicService.java
+++ src/main/java/com/rohitksingh/springbootjavabrains/TopicService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicService {
@@ -27,10 +28,24 @@ public class TopicService {
     }
 
     public Topic getTopic(int id){
-        return topics.stream().filter(t-> t.getId()==id).findFirst().get();
+        Topic topic =null;
+        return topic = (topicRepository.findById(id)!=null) ? topic : null;
     }
 
     public void addTopic(Topic topic){
         topicRepository.save(topic);
     }
+
+    public void updateTopic(Topic topic){
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(Topic topic){
+        topicRepository.delete(topic);
+    }
+
+    public void deleteAllTopics(){
+        topicRepository.deleteAll();
+    }
+
 }
