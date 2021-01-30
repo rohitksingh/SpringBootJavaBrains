d66e549f06cd23555b8576d647d98c43cf406d00
Rohit <r4rohit002@gmail.com>
2021-01-29 23:59:07 -0700

    Created Maven project

---
+++ src/main/java/com/rohitksingh/springbootjavabrains/SpringbootjavabrainsApplication.java
@@ -0,0 +1,13 @@
+package com.rohitksingh.springbootjavabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjavabrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjavabrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohitksingh/springbootjavabrains/SpringbootjavabrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitksingh.springbootjavabrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjavabrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
